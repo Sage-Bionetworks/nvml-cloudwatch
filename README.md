dbd1a3f02f2a6517d53ee38ce2a5c46e80f3fec9
bhoff <bruce.hoff@sagebase.org>
2017-02-11 11:23:06 -0800

    updates

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
 # nvml-cloudwatch
 This utility collects GPU metrics from NVIDIA management library (NVML) and sends them to CloudWatch
-```
-Run using `nvidia-docker`: https://github.com/NVIDIA/nvidia-docker
 
-nvidia-docker run -d \
--v /path/to/aws/creds:/root/.aws/credentials:ro \
---restart unless-stopped \
+```
+docker run -d \
+-e aws_access_key_id=$cloudwatch_aws_access_key_id \
+-e aws_secret_access_key=$cloudwatch_aws_secret_access_key \
+--device /dev/nvidia0:/dev/nvidia0 \
+--device /dev/nvidia1:/dev/nvidia1 \
+--device /dev/nvidia2:/dev/nvidia2 \
+--device /dev/nvidia3:/dev/nvidia3 \
+--device /dev/nvidiactl:/dev/nvidiactl \
+--device /dev/nvidia-uvm:/dev/nvidia-uvm \
+-v /usr/lib64/nvidia/libnvidia-ml.so:/usr/lib64/libnvidia-ml.so:ro \
+-v /usr/lib64/nvidia/libnvidia-ml.so.1:/usr/lib64/libnvidia-ml.so.1:ro \
+-v /usr/lib64/nvidia/libnvidia-ml.so.367.48:/usr/lib64/libnvidia-ml.so.367.48:ro \
 --cpuset-cpus "0" \
 --memory 512m \
 --memory-swap 0m \
 -h $(docker-machine active) \
+--restart unless-stopped \
 --name nvml-cloudwatch docker.synapse.org/syn5644795/nvml-cloudwatch 
-
+```
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/local/bin/python2.7
 # encoding: utf-8
 from pynvml import *
-import time
 import os
 import boto3
 import signal
+import time
 
 def signal_handler(signal, frame):
     print('Shutting down.')
@@ -13,30 +13,30 @@ def signal_handler(signal, frame):
 def main(argv=None):
     nvmlInit()
     #c = statsd.StatsClient(os.environ['statsd_host'], 8125)
-    cwc = boto3.client('cloudwatch', region_name='us-east-1')
+    cwc = boto3.client('cloudwatch', aws_access_key_id=os.environ['aws_access_key_id'], aws_secret_access_key=os.environ['aws_secret_access_key'], region_name='us-east-1')
 
     
     print "Driver Version:", nvmlSystemGetDriverVersion()
     deviceCount = nvmlDeviceGetCount()
-    host=os.getenv("host")
+    host=os.getenv('HOSTNAME')
 
     while (True):
         for i in range(deviceCount):
             handle = nvmlDeviceGetHandleByIndex(i)
             info = nvmlDeviceGetMemoryInfo(handle)
             utilizationInfo = nvmlDeviceGetUtilizationRates(handle)
-            cwc.put_metric_data(Namespace=host,MetricData=[
-                {'MetricName':'gpu.memory.total-'+str(i),'Dimensions':[{"host":host}],'Value':info.total,'Unit':'Bytes'},
-                {'MetricName':"gpu.memory.free-"+str(i), 'Dimensions':[{"host":host}],'Value':info.free, 'Unit':'Bytes'},
-                {'MetricName':"gpu.memory.used-"+str(i),'Dimensions':[{"host":host}],'Value':info.used,'Unit':'Bytes'},
-                {'MetricName':"gpu.power.state-"+str(i),'Dimensions':[{"host":host}],'Value':nvmlDeviceGetPerformanceState(handle)},
-                {'MetricName':"gpu.power.usage-"+str(i),'Dimensions':[{"host":host}],'Value':nvmlDeviceGetPowerUsage(handle)},
-                {'MetricName':"gpu.temperature-"+str(i),'Dimensions':[{"host":host}],'Value':nvmlDeviceGetTemperature(handle, NVML_TEMPERATURE_GPU)},
-                {'MetricName':"gpu.clock.graphics-"+str(i),'Dimensions':[{"host":host}],'Value':nvmlDeviceGetClockInfo(handle, NVML_CLOCK_GRAPHICS)},
-                {'MetricName':"gpu.clock.sm-"+str(i),'Dimensions':[{"host":host}],'Value':nvmlDeviceGetClockInfo(handle, NVML_CLOCK_SM)},
-                {'MetricName':'gpu.clock.mem-'+str(i),'Dimensions':[{"host":host}],'Value':nvmlDeviceGetClockInfo(handle, NVML_CLOCK_MEM),'Unit':'Bytes'},
-                {'MetricName':"gpu.utilization.gpu-"+str(i),'Dimensions':[{"host":host}],'Value':utilizationInfo.gpu,'Unit':'Bytes'},
-                {'MetricName':"gpu.utilization.memory-"+str(i),'Dimensions':[{"host":host}],'Value':utilizationInfo.memory,'Unit':'Bytes'}
+            cwc.put_metric_data(Namespace='nvidia',MetricData=[
+                {'MetricName':'gpu.memory.total','Dimensions':[{'Name':'host','Value':host},{'Name':'device','Value':str(i)}],'Value':info.total,'Unit':'Bytes'},
+                {'MetricName':"gpu.memory.free", 'Dimensions':[{'Name':'host','Value':host},{'Name':'device','Value':str(i)}],'Value':info.free, 'Unit':'Bytes'},
+                {'MetricName':"gpu.memory.used",'Dimensions':[{'Name':'host','Value':host},{'Name':'device','Value':str(i)}],'Value':info.used,'Unit':'Bytes'},
+                {'MetricName':"gpu.power.state",'Dimensions':[{'Name':'host','Value':host},{'Name':'device','Value':str(i)}],'Value':nvmlDeviceGetPerformanceState(handle)},
+                {'MetricName':"gpu.power.usage",'Dimensions':[{'Name':'host','Value':host},{'Name':'device','Value':str(i)}],'Value':nvmlDeviceGetPowerUsage(handle)},
+                {'MetricName':"gpu.temperature",'Dimensions':[{'Name':'host','Value':host},{'Name':'device','Value':str(i)}],'Value':nvmlDeviceGetTemperature(handle, NVML_TEMPERATURE_GPU)},
+                {'MetricName':"gpu.clock.graphics",'Dimensions':[{'Name':'host','Value':host},{'Name':'device','Value':str(i)}],'Value':nvmlDeviceGetClockInfo(handle, NVML_CLOCK_GRAPHICS)},
+                {'MetricName':"gpu.clock.sm",'Dimensions':[{'Name':'host','Value':host},{'Name':'device','Value':str(i)}],'Value':nvmlDeviceGetClockInfo(handle, NVML_CLOCK_SM)},
+                {'MetricName':'gpu.clock.mem','Dimensions':[{'Name':'host','Value':host},{'Name':'device','Value':str(i)}],'Value':nvmlDeviceGetClockInfo(handle, NVML_CLOCK_MEM),'Unit':'Bytes'},
+                {'MetricName':"gpu.utilization.gpu",'Dimensions':[{'Name':'host','Value':host},{'Name':'device','Value':str(i)}],'Value':utilizationInfo.gpu,'Unit':'Bytes'},
+                {'MetricName':"gpu.utilization.memory",'Dimensions':[{'Name':'host','Value':host},{'Name':'device','Value':str(i)}],'Value':utilizationInfo.memory,'Unit':'Bytes'}
             ])
     
         print "NVML metrics pushed to CloudWatch"
